6674738770e4e46665d1237dbb9737c14a8b6b35
Carinalinda <catcatarina193@gmail.com>
2024-09-10 20:57:19 -0300

    Iniciando codigo

---
+++ Bombacombustivel.py
@@ -0,0 +1,21 @@
+class BombaCombustivel:
+    def __init__(self, valor_litro, quantidade_disponivel): # _int_Ela configura os vals da bomba, preço por litro quantidade disponível.
+        self.valor_litro = valor_litro
+        self.quantidade_disponivel = quantidade_disponivel
+
+    def abastecer_por_valor(self, valor):
+        litros_abastecidos = valor / self.valor_litro
+        if litros_abastecidos <= self.quantidade_disponivel:
+            self.quantidade_disponivel -= litros_abastecidos
+            return litros_abastecidos
+        else:
+            return "Quantidade insuficiente na bomba."
+
+    def abastecer_por_litro(self, litros):
+        if litros <= self.quantidade_disponivel:
+            self.quantidade_disponivel -= litros
+            return litros * self.valor_litro
+        else:
+            return "Quantidade insuficiente na bomba."
+
+        print(bomba.quantidade_disponivel)
